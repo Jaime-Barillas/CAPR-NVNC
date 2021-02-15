8004f54ff7c194d6af4524506b820fcf84e4bfa6
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2021-02-15 16:17:54 -0500

    Write list of objects to JSON file.

--- src/capr_nvnc/core.clj
+++ src/capr_nvnc/core.clj
@@ -1,12 +1,12 @@
 (ns capr-nvnc.core
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [jsonista.core :as json])
   (:gen-class))
 
-(defn get-file-text [file-path]
-  (let [file (.getCanonicalFile (io/file file-path))]
-    (if (.exists file)
-      (slurp file)
-      (throw (Exception. "File does not exist!")))))
+(defn get-file-text [file]
+  (if (.exists file)
+    (slurp file)
+    (throw (Exception. "File does not exist!"))))
 
 (defn split-at-dialogue [story]
   (.split story " (?=\")|(?<=[\\.?!]\") "))
@@ -27,9 +27,18 @@
     #"\w" (make-narration block)
     #"\"" (make-dialogue block)))
 
+(defn emit-json [file objs]
+  (json/write-value file objs))
+
+(defn convert [file-path]
+  (let [file (io/file file-path)
+        out-file-name (.replaceFirst (.getName file) "\\.[a-zA-Z]+$" ".json")]
+    (->> file
+         get-file-text
+         split-at-dialogue
+         (mapv make-object)
+         (emit-json (io/file out-file-name)))))
+
 (defn -main
   [& args]
-  (prn (->> (first args)
-            get-file-text
-            split-at-dialogue
-            (map make-object))))
+  (prn (convert (first args))))
