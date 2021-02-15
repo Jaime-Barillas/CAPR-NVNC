d26838a358d7cb34e3f53aceb15074903cdb2ab2
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2021-02-15 14:25:55 -0500

    Read file specified by cli arg.

--- src/capr_nvnc/core.clj
+++ src/capr_nvnc/core.clj
@@ -1,7 +1,13 @@
 (ns capr-nvnc.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
+(defn get-file-text [file-path]
+  (let [file (.getCanonicalFile (io/file file-path))]
+    (if (.exists file)
+      (slurp file)
+      (throw (Exception. "File does not exist!")))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (prn (get-file-text (first args))))
